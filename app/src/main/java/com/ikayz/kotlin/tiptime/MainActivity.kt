9a9901c5e126532b60fd2d6fe9f60f3caa0c3ee5
Isaac Miti <ikayz@live.com>
2022-03-04 15:51:03 +0200

    Add tip and round up

--- app/src/main/java/com/ikayz/kotlin/tiptime/MainActivity.kt
+++ app/src/main/java/com/ikayz/kotlin/tiptime/MainActivity.kt
@@ -27,5 +27,7 @@ class MainActivity : AppCompatActivity() {
             R.id.option_eighteen_percent -> 0.18
             else -> 0.15
         }
+        var tip = tipPercentage * cost
+        val roundUp = binding.roundUpSwitch.isChecked
     }
 }
