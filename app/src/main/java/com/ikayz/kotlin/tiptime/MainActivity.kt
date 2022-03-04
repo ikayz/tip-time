88aef4af20fbd6bde49829a4406cdc80a2129274
Isaac Miti <ikayz@live.com>
2022-03-04 15:43:26 +0200

    Add binding when statement

--- app/src/main/java/com/ikayz/kotlin/tiptime/MainActivity.kt
+++ app/src/main/java/com/ikayz/kotlin/tiptime/MainActivity.kt
@@ -12,5 +12,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.calculateButton.setOnClickListener {
+            calculateTip()
+        }
+    }
+
+    fun calculateTip() {
+        val stringInTextField = binding.costOfService.text.toString()
+        val cost = stringInTextField.toDouble()
+        val selectedId = binding.tipOptions.checkedRadioButtonId
+        val tipPercentage = when(selectedId) {
+            R.id.option_twenty_percent -> 0.20
+            R.id.option_eighteen_percent -> 0.18
+            else -> 0.15
+        }
     }
 }
